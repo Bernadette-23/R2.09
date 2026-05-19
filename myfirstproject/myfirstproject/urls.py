7580b581d26016430f20bf84aacf4aadd36d2d9a
Bernadette-23 <lafagebernadette23@gmail.com>
2026-05-19 09:42:33 +0200

    maj 2e CRUD playlist

--- myfirstproject/myfirstproject/urls.py
+++ myfirstproject/myfirstproject/urls.py
@@ -20,5 +20,6 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('myfirstapp/', include('myfirstapp.urls')),
-    path('bibliotheque/', include('bibliotheque.urls'))
+    path('bibliotheque/', include('bibliotheque.urls')),
+    path('playlist/', include('playlist.urls')),
 ]
